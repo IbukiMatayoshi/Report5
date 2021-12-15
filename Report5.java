9d8991f399f8cf8974cf0fb7090d522bf70d684c
Matayoshi IBUKI <e215712@ie.u-ryukyu.ac.jp>
2021-12-15 09:51:45 +0900

    3rd commit for Report5

--- Report5.java
+++ Report5.java
@@ -1,12 +1,9 @@
 public class Report5{
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException nullE){
-            System.out.println("nullを参照してエラーが発生しています！");
-            System.out.println(nullE.getMessage());
-        }
+        String str = "百二十三";
+
+        int value = Integer.parseInt(str);
+
         
     }
 }
